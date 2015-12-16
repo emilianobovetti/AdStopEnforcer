e4790caf116c72228f091d114110936b4b114244
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2015-12-16 16:34:03 +0100

    indentation

--- js/inject.js
+++ js/inject.js
@@ -17,23 +17,24 @@
  */
 
 var bannedWindowProperties = [
-  'fuckAdBlock',
-  'is_adblock_detect'
+    'fuckAdBlock',
+    'is_adblock_detect'
 ];
 
 function getScriptElement(name) {
-  var script       = document.createElement('script');
-  script.innerHTML = "Object.defineProperty(window, '" + name + "', { value: null, writable: false, configurable: false });";
-  return script;
+    var script       = document.createElement('script');
+    script.innerHTML = "Object.defineProperty(window, '" + name + "', { value: null, writable: false, configurable: false });";
+    return script;
 }
 
 function injectScript() {
-  if (document.head) {
-    bannedWindowProperties.forEach(function(property) {
-      document.head.appendChild(getScriptElement(property));
-    });
-    clearInterval(injectCheck);
-  }
+    if (document.head) {
+        bannedWindowProperties.forEach(function(property) {
+            document.head.appendChild(getScriptElement(property));
+        });
+
+        clearInterval(injectCheck);
+    }
 }
 
 var injectCheck = setInterval(injectScript, 1);
